3d90897c58069c3b797a6d2a414c3a1416e7add5
Jyeongjae <lovej2078@gmail.com>
2024-10-07 20:56:42 +0900

    set authentication to new_post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from lib2to3.fixes.fix_input import context
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from unicodedata import category
 
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -30,7 +31,7 @@ def tag_page(request, slug):
             'tag': tag,
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
-            
+
         }
     )
 
@@ -53,6 +54,21 @@ def category_page(request, slug):
         }
     )
 
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
 class PostDetail(DetailView):
     model = Post
     def get_context_data(self, **kwargs):
@@ -60,24 +76,3 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-# def index(request):
-#     posts = Post.objects.all().order_by("-pk")
-#
-#     return render(
-#         request,
-#         "blog/post_list.html",
-#         {
-#             "posts": posts,
-#         }
-#     )
-
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-#
-#     return render(
-#         request,
-#         "blog/post_detail.html",
-#         {
-#             "post": post,
-#         }
-#     )
